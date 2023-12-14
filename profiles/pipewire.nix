76fa5b1aa84d2c49f7e567e8fdaca534f6a0f006
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2023-12-14 11:44:08 +0100

    remove pipewire low latency config

--- profiles/pipewire.nix
+++ profiles/pipewire.nix
@@ -12,7 +12,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -21,40 +21,40 @@
     wireplumber.enable = true;
   };
   environment.etc = let json = pkgs.formats.json {}; in {
-	"wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-		bluez_monitor.properties = {
-			["bluez5.enable-sbc-xq"] = true,
-			["bluez5.enable-msbc"] = true,
-			["bluez5.enable-hw-volume"] = true,
-			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-		}
-	'';
-    "pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
-      context.properties = {
-        default.clock.rate = 48000
-        default.clock.quantum = 128
-        default.clock.min-quantum = 128
-        default.clock.max-quantum = 256
-      }
-    '';
-	"pipewire/pipewire-pulse.d/92-low-latency.conf".source = json.generate "92-low-latency.conf" {
-      context.modules = [
-        {
-          name = "libpipewire-module-protocol-pulse";
-          args = {
-            pulse.min.req = "128/48000";
-            pulse.default.req = "128/48000";
-            pulse.max.req = "256/48000";
-            pulse.min.quantum = "128/48000";
-            pulse.max.quantum = "256/48000";
-          };
-        }
-      ];
-      stream.properties = {
-        node.latency = "64/48000";
-        resample.quality = 1;
-      };
-    };
+	  "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+		  bluez_monitor.properties = {
+			  ["bluez5.enable-sbc-xq"] = true,
+  			["bluez5.enable-msbc"] = true,
+	  		["bluez5.enable-hw-volume"] = true,
+		  	["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+  		}
+	  '';
+    # "pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
+    #   context.properties = {
+    #     default.clock.rate = 48000
+    #     default.clock.quantum = 128
+    #     default.clock.min-quantum = 128
+    #     default.clock.max-quantum = 256
+    #   }
+    # '';
+  	# "pipewire/pipewire-pulse.d/92-low-latency.conf".source = json.generate "92-low-latency.conf" {
+    #   context.modules = [
+    #     {
+    #       name = "libpipewire-module-protocol-pulse";
+    #       args = {
+    #         pulse.min.req = "128/48000";
+    #         pulse.default.req = "128/48000";
+    #         pulse.max.req = "256/48000";
+    #         pulse.min.quantum = "128/48000";
+    #         pulse.max.quantum = "256/48000";
+    #       };
+    #     }
+    #   ];
+    #   stream.properties = {
+    #     node.latency = "64/48000";
+    #     resample.quality = 1;
+    #   };
+    # };
   };
   environment.systemPackages = with pkgs; [
     helvum
