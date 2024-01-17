2340ae8a47d73d6c4376cd00781d8040b0945ef2
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-01-17 11:16:58 +0100

    add ffmpeg-custom

---
+++ packages/development/ffmpeg/default.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> {};
+
+# For systems that don’t support flakes
+{
+  ffmpeg-custom =
+    let
+      overrides = {
+        nonfreeLicensing = true;
+        fdkaacExtlib = true;
+      };
+
+      attrs = {
+        name="ffmpeg-custom" + ffmpeg.version;
+      };
+    in
+    ffmpeg-full.override overrides // attrs;
+}
---
+++ packages/development/ffmpeg/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A full build of ffmpeg, including the proprietary stuff";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        overrides = {
+          ffmpeg = pkgs.ffmpeg_5;
+          nonfreeLicensing = true;
+          fdkaacExtlib = true;
+        };
+      in
+      rec {
+        packages.ffmpeg-custom = pkgs.ffmpeg-full.override overrides;
+        defaultPackage = packages.ffmpeg-custom;
+      }
+    );
+}
