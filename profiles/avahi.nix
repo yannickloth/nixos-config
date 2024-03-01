4c37b1d37b3fc0252a0e23089648a222c74665eb
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-03-01 09:51:32 +0100

    fix config for nssmdns: use nssmdns4 config instead

--- profiles/avahi.nix
+++ profiles/avahi.nix
@@ -6,7 +6,7 @@ with lib;
     enable = true;
     # Important to resolve .local domains of printers, otherwise you get an error
     # like  "Impossible to connect to XXX.local: Name or service not known"
-    nssmdns = true;
+    nssmdns4 = true;
     openFirewall = true;
     browseDomains = [ "fritz.box" "local" "home" ];
   };
