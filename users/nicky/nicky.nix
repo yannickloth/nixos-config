d482f69f8292b0db7d03532971c6a96f463a0263
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-03-26 12:14:23 +0100

    add doc to groups of nicky

--- users/nicky/nicky.nix
+++ users/nicky/nicky.nix
@@ -14,7 +14,14 @@ in
       hashedPassword = "$2b$05$.x8gLS6s8Vd2qFsPFnoBjewjLxf6bcuW/EM1TnPohnNmI0LRHdQ/q";
       isNormalUser = true;
       description = userDescription;
-      extraGroups = [ "libvirtd" "lp" "networkmanager" "podman" "scanner" "vboxusers" "wheel" "yubikey" ];
+      extraGroups = [ "libvirtd" 
+                      "lp" # for scanning
+                      "networkmanager"
+                      "podman"
+                      "scanner" # for scanning
+                      "vboxusers"
+                      "wheel"
+                      "yubikey" ];
       group = username;
     };
   };  
