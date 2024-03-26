6d591622adf347f982187481fa4ed4311921d5a0
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-03-26 13:35:15 +0100

    use airscan instead of escl, allow ipp over usb

--- profiles/scanning.nix
+++ profiles/scanning.nix
@@ -12,9 +12,13 @@ in
           MFCL2700DW = { model = "${mfcl2700dw_model}"; ip = "${mfcl2700dw_ip}"; };
         };
       };
-    # extraBackends = [ pkgs.sane-airscan pkgs.hplipWithPlugin ];
+    disabledDefaultBackends = [ "escl" ]; # disable the default escl backend, to avoid finding scanners twice (once with escl, once with airscan)
+    extraBackends = [ pkgs.sane-airscan # for Apple AirScan scanners and Microsoft WSD
+                      #pkgs.hplipWithPlugin
+    ];
     openFirewall = true;
   };
 
-  services.saned.enable = true;
+  services.ipp-usb.enable=true; # enable ipp-usb, a daemon to turn an USB printer/scanner supporting IPP everywhere (aka AirPrint, WSD, AirScan) into a locally accessible network printer/scanner
+  services.saned.enable = true; # Enable saned network daemon for remote connection to scanners.
 }
