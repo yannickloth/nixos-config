a94afd8aaf7611563165f14be19bf7046a3eecf7
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-04-13 13:15:02 +0200

    fix displayManager config

--- profiles/xserver.nix
+++ profiles/xserver.nix
@@ -6,53 +6,76 @@ with lib;
 
 {
   config = {
-    programs.ssh.askPassword = lib.mkForce "${pkgs.ksshaskpass}/bin/ksshaskpass"; # resolves the conflict between seahorse (gnome) and ksshaskpass (plasma). Is just useful if both KDE and Gnome are installed.
-    services.desktopManager.plasma6.enable = true; # Enable the KDE Plasma 6 Desktop Environment.
-    # enable xserver on workstations
-    services.xserver = {
-      # By default, enable the X11 windowing system
-      #enable = mkDefault true;
-      enable = true;
-      autorun = true;
-
-      # Export configuration, so it's easier to debug
-      exportConfiguration = true;
-
-      # Configure keymap in X11
-      xkb.layout = "be";
-      xkb.variant = "";
-
-      desktopManager = {
-       gnome = {
-          enable = true;
-        };
-        # Enable the KDE Plasma Desktop Environment.
-        # plasma5 = {
-        #   enable = false;
-        # };
-        xterm.enable = false;
+    programs = {
+      dconf.enable = mkDefault true; # enable dconf support on all workstations for storage of configration
+      # seahorse.enable = false;
+      ssh.askPassword = lib.mkForce "${pkgs.ksshaskpass}/bin/ksshaskpass"; # resolves the conflict between seahorse (gnome) and ksshaskpass (plasma). Is just useful if both KDE and Gnome are installed.
+    };
+    services = {
+      desktopManager={
+        plasma6.enable = true; # Enable the KDE Plasma 6 Desktop Environment.
       };
       displayManager = {
-        gdm = {
-          enable = false;
-        };
-        sddm = {
+        defaultSession = "plasma";# = "gnome";
+        sddm={
           autoNumlock = true;
           enable = true;
           wayland.enable = false;
         };
-        defaultSession = "plasma";# = "gnome";
+      };
+      # gnome.gnome-keyring.enable = lib.mkForce false;
+      
+      # enable xserver on workstations
+      xserver = {
+        # By default, enable the X11 windowing system
+        # enable = mkDefault true;
+        enable = true;
+        autorun = true;
+
+        # Export configuration, so it's easier to debug
+        exportConfiguration = true;
+
+        # Configure keymap in X11
+        xkb.layout = "be";
+        xkb.variant = "";
+
+        # Enable touchpad support (enabled default in most desktopManager).
+        libinput.enable = lib.mkDefault true;
+      
+        desktopManager = {
+          gnome = {
+            enable = false;
+          };
+          xterm.enable = false;
+        };
+        displayManager = {
+          gdm = {
+            enable = false;
+          };
+        };
       };
     };
-    services.gnome.gnome-keyring.enable = lib.mkForce false;
-    programs.seahorse.enable = false;
+    
     security = {
       pam.services.aeiuno.enableKwallet = true;
       pam.services.nicky.enableKwallet = true;
       #pam.services.aeiuno.enableGnomeKeyring = true;
       #pam.services.nicky.enableGnomeKeyring = true;
     };
+
     fonts = {
+      enableDefaultPackages = true;
+      fontconfig = {
+        enable = mkForce true;
+        defaultFonts = {
+          monospace = ["Roboto Mono 13"];
+          sansSerif = ["Roboto 13"];
+          serif = ["Roboto Slab 13"];
+        };
+      };
+      fontDir = {
+        enable = true;
+      };
       packages = with pkgs; [
         caladea
         carlito
@@ -89,24 +112,6 @@ with lib;
         vistafonts
         # whatsapp-emoji-font
       ];
-      fontconfig = {
-        enable = mkForce true;
-        defaultFonts = {
-          monospace = ["Roboto Mono 13"];
-          sansSerif = ["Roboto 13"];
-          serif = ["Roboto Slab 13"];
-        };
-      };
-      fontDir = {
-        enable = true;
-      };
-      enableDefaultPackages = true;
     };
-
-    # enable dconf support on all workstations for storage of configration
-    programs.dconf.enable = mkDefault true;
-
-    # Enable touchpad support (enabled default in most desktopManager).
-      services.xserver.libinput.enable = lib.mkDefault true;
-    };
+  };
 }
