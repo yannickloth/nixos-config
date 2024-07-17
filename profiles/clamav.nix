4a955052cfc20b5a9b19e66b71ef0cfde45f0e62
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-07-17 09:48:47 +0200

    disable clamav daemon

--- profiles/clamav.nix
+++ profiles/clamav.nix
@@ -4,7 +4,7 @@ with lib;
 {
   # Enable the ClamAV service and keep the database up to date
   services.clamav = {
-    daemon.enable = true;
+    daemon.enable = false;
     updater.enable = true;
   };
 }
