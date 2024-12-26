e0c1e1dc1fd469c67dd1724afbd787bd6f6bc028
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-12-26 15:41:31 +0100

    update NerdFonts packages in console.nix

--- profiles/console.nix
+++ profiles/console.nix
@@ -7,8 +7,8 @@ with lib;
     enable = true; # Use kmscon as the virtual console instead of gettys.
     hwRender = true; # Whether to use 3D hardware acceleration to render the console.
     fonts = [
-      { name = "FiraCode Nerd Font Mono"; package = pkgs.fira-code-nerdfont; }
-      { name = "Inconsolata Nerd Font Mono"; package = pkgs.inconsolata-nerdfont; }
+      { name = "FiraCode Nerd Font Mono"; package = pkgs.nerd-fonts.fira-code; }
+      { name = "Inconsolata Nerd Font Mono"; package = pkgs.nerd-fonts.inconsolata; }
       { name = "Source Code Pro"; package = pkgs.source-code-pro; }
     ];
     extraConfig = "xkb-layout=be";
