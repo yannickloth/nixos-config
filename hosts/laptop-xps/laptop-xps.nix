9bd4fbdbded016edd27c096cac605bf49a03eaa2
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-12-26 17:34:59 +0100

    add nix.settings config

--- hosts/laptop-xps/laptop-xps.nix
+++ hosts/laptop-xps/laptop-xps.nix
@@ -59,6 +59,7 @@ with lib;
       ../../profiles/sudo.nix
       #../../profiles/syncthing/syncthing.nix
       ../../profiles/tor.nix
+      ../../profiles/typst.nix
       ../../profiles/virtualbox.nix
       ../../profiles/waydroid.nix
       ../../profiles/xserver.nix
@@ -76,4 +77,19 @@ with lib;
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  nix.settings = {
+    cores = 0; # This option defines the maximum number of concurrent tasks during one build. It affects, e.g., -j option for make. The special value 0 means that the builder should use all available CPU cores in the system. Some builds may become non-deterministic with this option; use with care! Packages will only be affected if enableParallelBuilding is set for them.
+    max-jobs = 4; # This option defines the maximum number of jobs that Nix will try to build in parallel. The default is auto, which means it will use all available logical cores. It is recommend to set it to the total number of logical cores in your system (e.g., 16 for two CPUs with 4 cores each and hyper-threading).
+    system-features = [
+      "benchmark" # May apply to packages or tests that depend on benchmarking features.
+      # "big-parallel" # Enables tasks designed for builds that heavily leverage parallelism (> 16 cores), but enabling it on a system with a low core count (e.g., 4 logical cores) can lead to inefficiencies and potential issues:
+      "cgroups" # Specifies that the system supports Linux cgroups (Control Groups), which are often used for resource isolation.
+      "kvm" # Indicates that the system can perform builds inside a KVM virtual machine.
+      "nixos" # Indicates that the system is running NixOS. This is automatically set on NixOS.
+      "nixos-test" # It allows for automated tests of NixOS modules, configurations, and services in virtual machines or containers. Tests typically run within QEMU virtual machines (or other supported backends) that emulate a full NixOS system.
+      "reproducible-paths" #     Ensures paths in builds are highly deterministic.
+      "sandbox" # Indicates that builds should be sandboxed. A sandboxed build means that the environment is completely isolated and cannot access the host filesystem or network, ensuring purity in builds.
+    ];
+  };
 }
