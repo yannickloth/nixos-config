c89520ceccacd536ed47e92cd1429fa88d0db4dc
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-12-26 14:26:35 +0100

    upgrade to JDK23

--- profiles/java.nix
+++ profiles/java.nix
@@ -4,7 +4,7 @@ with lib;
 {
   programs.java = {
     enable = true; # Install and setup the Java development kit. This adds JAVA_HOME to the global environment, by sourcing the jdk’s setup-hook on shell init.
-    package = pkgs.jdk22; # The jdk package to use.
+    package = pkgs.jdk23; # The jdk package to use.
     binfmt = true; # Whether to enable binfmt to execute java jar’s and classes.
   };
 }
