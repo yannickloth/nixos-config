d5615fe064beef79335f3008f1a873f2c9a4392d
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-12-27 16:17:40 +0100

    refactor printing and scanning

--- profiles/printing.nix
+++ profiles/printing.nix
@@ -1,42 +1,12 @@
 { pkgs, ... }:
-let
-  mfcl2700dw = "Brother MFC L2700DW";
-  mfcl2700dw_name = "MFC-L2700DW";
-  mfcl2700dw_ip = "192.168.190.27";
-  xp15000 = "Epson XP 15000";
-  xp15000_name = "XP-15000";
-  xp15000_ip = "192.168.190.25";
-in
+
 {
+  programs.system-config-printer.enable=true;
   services.ipp-usb.enable=true;
   services.printing = {
     enable = true;
-    # Enable CUPS to print documents.
     drivers = with pkgs; [ epson-escpr2 brgenml1lpr brgenml1cupswrapper brlaser canon-cups-ufr2 ]; #hplipWithPlugin
-    #cups-pdf.instances.defaultcupspdf.enable = true;
-    cups-pdf.enable = true;
-  };
-  programs.system-config-printer.enable=true;
-  hardware = {
-    printers = {
-      ensureDefaultPrinter = "Brother_MFC-L2700DW";
-#       ensurePrinters = [
-#         {
-#           name = mfcl2700dw_name;
-#           deviceUri = "ipp://${mfcl2700dw_ip}/ipp";
-#           model = "everywhere";
-#           description = pkgs.lib.replaceStrings [ "_" ] [ " " ] mfcl2700dw;
-#           location = "B31 1 Bureau";
-#         }
-#         {
-#           name = xp15000;
-#           deviceUri = "https://${xp15000_ip}:631/ipp/print";
-# #           model = "escpr2";
-#           model = "everywhere";
-#           description = pkgs.lib.replaceStrings [ "_" ] [ " " ] xp15000;
-#           location = "B31 0 SàM";
-#         }
-#       ];
-    };
+    #cups-pdf.instances.defaultcupspdf.enable = true; 
+    cups-pdf.enable = true; # Enable CUPS to print documents.
   };
 }
--- profiles/scanning.nix
+++ profiles/scanning.nix
@@ -1,24 +1,29 @@
 { pkgs, ... }:
-let
-  mfcl2700dw_ip = "192.168.190.27";
-  mfcl2700dw_model = "MFC-L2700DW";
-in
 {
-  hardware.sane = {
-    enable = true;
-    brscan4 = {
-        enable = true;
-        netDevices = {
-          MFCL2700DW = { model = "${mfcl2700dw_model}"; ip = "${mfcl2700dw_ip}"; };
-        };
-      };
-    disabledDefaultBackends = [ "escl" ]; # disable the default escl backend, to avoid finding scanners twice (once with escl, once with airscan)
-    extraBackends = [ pkgs.sane-airscan # for Apple AirScan scanners and Microsoft WSD
-                      #pkgs.hplipWithPlugin
-    ];
-    openFirewall = true;
+  environment.systemPackages = with pkgs; [
+    naps2 # Scan documents to PDF and more, as simply as possible.
+    simple-scan
+  ];
+  nixpkgs.config.permittedInsecurePackages = [
+    "dotnet-sdk-6.0.428" # marked as insecure, refusing to evaluate, needed for naps2
+  ];
+
+  hardware = {
+    sane = {
+      enable = true;
+      # eSCL is a driverless scanning protocol. It is an http(s) and xml based for all communiction. the standard was created by Mopria.
+      # eSCL is the default communication method found in MacOS, and is often referred to as AirPrint Scanning or AirScan.
+      # On Linux there are two different back-ends for SANE to support eSCL: SANE-AirScan and SANE-eSCL.
+      # SANE-AirScan also supports the WSD protocol, thus it supports more than SANE-eSCL.
+      disabledDefaultBackends = [ "escl" ]; # disable the default sane-escl backend, to avoid finding scanners twice (once with sane-escl, once with sane-airscan)
+      extraBackends = [
+        pkgs.sane-airscan # for Apple AirScan scanners and Microsoft WSD
+        #pkgs.hplipWithPlugin
+      ];
+      openFirewall = true;
+    };
   };
 
-  services.ipp-usb.enable=true; # enable ipp-usb, a daemon to turn an USB printer/scanner supporting IPP everywhere (aka AirPrint, WSD, AirScan) into a locally accessible network printer/scanner
+  services.ipp-usb.enable = true; # enable ipp-usb, a daemon to turn an USB printer/scanner supporting IPP everywhere (aka AirPrint, WSD, AirScan) into a locally accessible network printer/scanner
   services.saned.enable = true; # Enable saned network daemon for remote connection to scanners.
 }
