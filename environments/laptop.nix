5cadfa28b51acff3baf797232ddb7edeeb9598f2
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-12-27 16:16:48 +0100

    disable captive portal stuff

--- environments/laptop.nix
+++ environments/laptop.nix
@@ -6,34 +6,28 @@ with lib;
 
 {
   imports =
-  [
-    ../profiles/laptop-firewall.nix
-  ];
+    [
+      ../profiles/laptop-firewall.nix
+    ];
   config = {
 
+    powerManagement = {
+      enable = mkDefault true; # enable suspend
+      powertop.enable = true; # Whether to enable powertop auto tuning on startup. Analyze power consumption on Intel-based laptops.
+    };
 
-    # enable suspend
-    powerManagement.enable = mkDefault true;
-
-    # for power optimizations
-    powerManagement.powertop.enable = true;
-
-    #services.low-battery-check = {
-      #enable = true;
-      #action = "hibernate";
+    #services.tlp = {
+      # Gnome 40 introduced a new way of managing power, without tlp.
+      # However, these 2 services clash when enabled simultaneously.
+      # https://github.com/NixOS/nixos-hardware/issues/260
+      # enable = lib.mkDefault ((lib.versionOlder (lib.versions.majorMinor lib.version) "21.05") || !config.services.power-profiles-daemon.enable); # Disabled. Enable this to keep TLP (absolutely needed for TTY-only sessions, where no Plasma session is running in parallel) and let it manage CPU freq, and let Plasma's PowerDevil just manage display, not the CPU.
     #};
-
-    # Gnome 40 introduced a new way of managing power, without tlp.
-    # However, these 2 services clash when enabled simultaneously.
-    # https://github.com/NixOS/nixos-hardware/issues/260
-    services.tlp.enable = lib.mkDefault ((lib.versionOlder (lib.versions.majorMinor lib.version) "21.05")
-                                       || !config.services.power-profiles-daemon.enable);
 
     # enable upower service for battery
-    #services.upower = {
-    #  enable = false; # Whether to enable Upower, a DBus service that provides power management support to applications.
-    #  criticalPowerAction = "Hibernate";
-    #};
+    services.upower = {
+      enable = true; # Enables battery monitoring daemon. Whether to enable Upower, a DBus service that provides power management support to applications.
+      #criticalPowerAction = "Hibernate"; #The action to take when timeAction or percentageAction has been reached for the batteries (UPS or laptop batteries) supplying the computer
+    };
 
     # Do not turn off when closing laptop lid
     services.logind.extraConfig = ''
@@ -43,10 +37,5 @@ with lib;
     # keep timezone updated to local time using geoclue
     time.timeZone = lib.mkForce "Europe/Luxembourg"; # force because somewhere else this is set to null, and Nix does not know how to resolve the conflict
     services.localtimed.enable = true;
-
-    # enable captive-browser, so we can connect to network that are secured
-    # by captive portal
-    programs.captive-browser.enable = true;
-    programs.captive-browser.interface = "wlp58s0";
   };
 }
