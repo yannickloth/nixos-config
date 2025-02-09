7143ceec100083d291c4c493f7f36544d39b0696
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2025-02-10 00:41:40 +0100

    reformat xps config

--- laptop-xps-configuration.nix
+++ laptop-xps-configuration.nix
@@ -273,8 +273,7 @@
       package = pkgs.ananicy-cpp;
     };
 
-    # Enable the OpenSSH daemon.
-    openssh.enable = true;
+    openssh.enable = true; # Enable the OpenSSH daemon.
     syncthing.openDefaultPorts = true;
     tailscale.enable = true;
   };
@@ -283,7 +282,7 @@
     enable = true; # Whether to enable xdg desktop integration.
     #     extraPortals = [
     #       pkgs.xdg-desktop-portal-gtk
-    #        pkgs.xdg-desktop-portal-wlr
+    #       pkgs.xdg-desktop-portal-wlr
     #     ];
     xdgOpenUsePortal = true; # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1 This will make xdg-open use the portal to open programs, which resolves bugs involving programs opening inside FHS envs or with unexpected env vars set from wrappers. See #160923 for more info.
   };
