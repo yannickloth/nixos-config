fb27fc9b592fcc6c8e31169410a0344f6cf151d9
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2025-02-14 21:51:27 +0100

    enable on-the-go specialisation

--- hosts/laptop-p16/hardware-configuration.nix
+++ hosts/laptop-p16/hardware-configuration.nix
@@ -157,14 +157,18 @@
       emulateWheel = lib.mkDefault config.hardware.trackpoint.enable;
     };
   };
-  # specialisation = {
-  #   on-the-go.configuration = {
-  #     system.nixos.tags = [ "on-the-go" ];
-  #     hardware.nvidia = {
-  #       prime.offload.enable = lib.mkForce true;
-  #       prime.offload.enableOffloadCmd = lib.mkForce true;
-  #       prime.sync.enable = lib.mkForce false;
-  #     };
-  #   };
-  # };
+  specialisation = {
+    on-the-go.configuration = {
+      system.nixos.tags = [ "on-the-go" ];
+      hardware.nvidia = {
+        prime = {
+          offload = {
+            enable = lib.mkForce true;
+            enableOffloadCmd = lib.mkForce true;
+          };
+          sync.enable = lib.mkForce false;
+        };
+      };
+    };
+  };
 }
