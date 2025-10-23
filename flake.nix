9656f6d538d4aabf804affd2b21e18a1d75cdf18
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2025-10-23 17:15:28 +0200

    switch to release 25.05

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
